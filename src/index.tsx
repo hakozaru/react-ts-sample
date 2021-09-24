7f17b6fa1983b9d54450fecb40a23f9106ed6465
hakozaru <t.morii@everyleaf.com>
2021-09-24 14:57:04 +0900

    本当にTSによってES5に変換されているか確認する

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom'
 import './reset.css'
 import Img from './example.png'
 
+const hoge = () => {
+  console.log('あああああああああ')
+}
+
 ReactDOM.render(
   <div>
     Hello world
     <img src={Img}></img>
+    <button onClick={hoge}></button>
   </div>,
   document.getElementById('root')
 )
